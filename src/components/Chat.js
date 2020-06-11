cc70af4b9327b461c147b03335b06932d8801274
Vazquez Cesar <cesar.evc@outlook.com>
2020-06-10 21:17:01 -0500

    initial commit

---
+++ src/components/Chat.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+function Chat(params) {
+    return (
+        <h1>CHAT</h1>
+    );
+}
+
+export default Chat;
---
+++ src/components/Join.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+function Join(params) {
+    return (
+        <h1>JOIN</h1>
+    );
+}
+
+export default Join;
