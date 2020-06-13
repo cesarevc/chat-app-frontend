343367db08a552301909f22136b2fc2000b40615
Vazquez Cesar <cesar.evc@outlook.com>
2020-06-13 16:38:06 -0500

    thats ok

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import queryString from 'query-string';
 import io from 'socket.io-client';
-// styles
-import './Chat.css';
 // components
 import InfoBar from '../InfoBar/InfoBar';
 import InputBox from '../InputBox/InputBox';
 import MessagesBox from '../MessagesBox/MessagesBox';
 import TextContainer from '../TextContainer/TextContainer';
+// styles
+import './Chat.css';
 
 let socket;
 
@@ -16,51 +16,36 @@ function Chat ({location}) {
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
     const [users, setUsers] = useState('');
-
-    const [messages, setMessages] = useState([]);
     const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
 
     const ENDPOINT = 'localhost:5000';
 
     
     useEffect(() => {
         const { name, room } = queryString.parse(location.search);
-
+    
         socket = io(ENDPOINT);
-
-        socket.emit('join', { name, room }, ( error ) => {
-            if(error) {
-                alert(error);
-            }
-        });
-        
-        socket.on('initial-message', (mssg) =>{
-            messages.push(mssg);
-            // setMessages([...messages, mssg]);
-        });
-        
-        socket.on("room-data", ({ users }) => {
-            setUsers(users);
-        });
-        
-        setName(name);
+    
         setRoom(room);
-
-        console.log("ENDPOINT LOCATION")
-    }, [ENDPOINT, location.search]);
-
+        setName(name)
+    
+        socket.emit('join', { name, room }, (error) => {
+          if(error) {
+            alert(error);
+          }
+        });
+      }, [ENDPOINT, location.search]);
 
     useEffect(() => {
-        socket.on('new-message', (mssg) => {
-            messages.push(mssg)
-            
-        }
-        // messages.push(mssg)
-        // setMessages([...messages, mssg])
-        );
+        socket.on('message', message => {
+          setMessages(messages => [ ...messages, message ]);
+        });
         
-        console.log('si se ejecuto perrote')
-    },[]);
+        socket.on("roomData", ({ users }) => {
+          setUsers(users);
+        });
+    }, []);
 
 
 
@@ -71,14 +56,9 @@ function Chat ({location}) {
         e.preventDefault();
 
         if(message) {
-            socket.emit('new-message', message, () => setMessage(''));            
-            // socket.on('message', (mssg) => messages.push(mssg))
-            
+            socket.emit('sendMessage', message, () => setMessage('') );            
         }
     }
-    
-    // console.log("message", message);
-    console.log("messages", messages);
 
 
     return (
@@ -87,7 +67,7 @@ function Chat ({location}) {
                 <InfoBar room={room} />
                 <MessagesBox messages={messages} name={name}/>
                 <InputBox message={message} setMessage={setMessage} sendMessage={sendMessage} />
-                <TextContainer   />
+                {/* <TextContainer   /> */}
             </div>
 
         </div>
--- src/components/MessagesBox/MessagesBox.js
+++ src/components/MessagesBox/MessagesBox.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import ScrollToBottom from 'react-scroll-to-bottom';
 // components
 import Message from '../Message/Message';
@@ -6,24 +6,12 @@ import Message from '../Message/Message';
 import './MessagesBox.css'
 
 function MessagesBox ({ messages, name }) {
-
-
-    useEffect(() => {
-        
-        hndlmsgs(messages)
-    },[messages]);
-
-    const hndlmsgs = (messages) => {
-        return (
-            <ScrollToBottom className="messages">
-                {messages.map((message, i) => <div key={i}><Message message={message} name={name} /></div>)}
-            </ScrollToBottom>
-        )
-    }
     
-    return hndlmsgs(messages)
-        
-    
+    return (
+        <ScrollToBottom className="messages">
+            {messages.map((message, i) => <div key={i}><Message message={message} name={name} /></div>)}
+        </ScrollToBottom>
+    )   
 }
 
 export default MessagesBox;
