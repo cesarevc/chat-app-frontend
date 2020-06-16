6a473f71c3c765d2aa0ae09796efac14321c2524
Vazquez Cesar <cesar.evc@outlook.com>
2020-06-16 17:38:45 -0500

    functionalities and README!

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -19,7 +19,7 @@ function Chat ({location}) {
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
 
-    const ENDPOINT = 'localhost:5000';
+    const ENDPOINT = 'https://chat-app-cesarevc.herokuapp.com/';
 
     
     useEffect(() => {
